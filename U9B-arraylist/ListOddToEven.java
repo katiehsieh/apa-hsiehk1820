1b91602fa5c6fe08d5d70a19c441526c00ff67a5
Katie Hsieh <katiehsieh25@gmail.com>
2019-02-27 23:50:32 -0800

    ListOddToEven

--- U9B-arraylist/ListOddToEven.java
+++ U9B-arraylist/ListOddToEven.java
@@ -14,24 +14,24 @@ public class ListOddToEven
     int evenPos = -1;
     
     for (int a = 0; a < ray.size(); a++) {
-    	if (Math.abs(ray.get(a)) % 2 != 0) {
-    		oddPos = a;
-    		
-    		for (int b = oddPos + 1; b < ray.size(); b++) {
-    			if (Math.abs(ray.get(b)) % 2 == 0) {
-    	    		evenPos = b;
-    	    	}
-    	    }
-    		
-    		break;
-    	}
+      if (Math.abs(ray.get(a)) % 2 != 0) {
+        oddPos = a;
+        
+        for (int b = oddPos + 1; b < ray.size(); b++) {
+          if (Math.abs(ray.get(b)) % 2 == 0) {
+            evenPos = b;
+          }
+        }
+        
+        break;
+      }
     }
     
     if (oddPos == -1 || evenPos == -1) {
-    	return -1;
+      return -1;
     }
     else {
-    	return evenPos - oddPos;
+      return evenPos - oddPos;
     }
     
   }
--- U9B-arraylist/OddToEvenRunner.java
+++ U9B-arraylist/OddToEvenRunner.java
@@ -10,7 +10,7 @@ public class OddToEvenRunner
 {
   public static void main( String args[] )
   {
-	  ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11));
-	  System.out.println(ListOddToEven.go(nums));
+    ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11));
+    System.out.println(ListOddToEven.go(nums));
   }
 }
