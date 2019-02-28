a3fdc1bfe48780e3574333b6a99023a99780916c
Katie Hsieh <katiehsieh25@gmail.com>
2019-02-27 23:26:21 -0800

    ListOddToEven

--- U9B-arraylist/ListOddToEven.java
+++ U9B-arraylist/ListOddToEven.java
@@ -8,8 +8,31 @@ import java.util.List;
 
 public class ListOddToEven
 {
-  public static boolean go( List<Integer> ray )
+  public static int go( List<Integer> ray )
   {
-    return false;
+    int oddPos = -1;
+    int evenPos = -1;
+    
+    for (int a = 0; a < ray.size(); a++) {
+    	if (Math.abs(ray.get(a)) % 2 != 0) {
+    		oddPos = a;
+    		
+    		for (int b = oddPos + 1; b < ray.size(); b++) {
+    			if (Math.abs(ray.get(b)) % 2 == 0) {
+    	    		evenPos = b;
+    	    	}
+    	    }
+    		
+    		break;
+    	}
+    }
+    
+    if (oddPos == -1 || evenPos == -1) {
+    	return -1;
+    }
+    else {
+    	return evenPos - oddPos;
+    }
+    
   }
 }
--- U9B-arraylist/OddToEvenRunner.java
+++ U9B-arraylist/OddToEvenRunner.java
@@ -1,11 +1,16 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Katie Hsieh
+//Date - 2-27-19
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OddToEvenRunner
 {
   public static void main( String args[] )
   {
+	  ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11));
+	  System.out.println(ListOddToEven.go(nums));
   }
 }
