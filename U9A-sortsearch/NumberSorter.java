8bd84bdd19ec46ca16fa2328bd9de3db9aafa91a
Katie Hsieh <katiehsieh25@gmail.com>
2019-02-28 01:00:50 -0800

    NumberSorter

--- U9A-sortsearch/NumberSorter.java
+++ U9A-sortsearch/NumberSorter.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Katie Hsieh
 
 import java.util.Arrays; 
 import java.util.Scanner;
@@ -10,17 +10,45 @@ import static java.lang.System.*;
 
 public class NumberSorter
 {
-	//instance variables and other methods not shown
+  //instance variables and other methods not shown
 
-	private static int getNumDigits(int number)
-	{
-		int count = 0;
-		return count;
-	}
+  private static int getNumDigits(int number)
+  {
+    int count = 0;
+    
+    while (number > 0) {
+      number = number / 10;
+      count++;
+    }
+    
+    return count;
+  }
 
-	public static int[] getSortedDigitArray(int number)
-	{
-		int[] sorted = null;
-		return sorted;
-	}
+  public static int[] getSortedDigitArray(int number)
+  {
+    int size = getNumDigits(number);
+    int[] input = new int[size];
+    
+    for (int i = 0; i < size; i++) {
+      input[i] = number % 10;
+      number = number / 10;
+    }
+    
+    // Insertion Sort
+    for (int a = 1; a < size; a++) {
+      int num = input[a];
+      int b = a;
+      
+      while (b > 0 && num < input[b-1]) {
+        // shift left until in correct spot
+        input[b] = input[b-1];
+        b--;
+      }
+      // switch values
+      input[b] = num;
+    }
+    
+    return input;
+  }
+  
 }
--- U9A-sortsearch/NumberSorterRunner.java
+++ U9A-sortsearch/NumberSorterRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Katie Hsieh
 
 import java.util.Arrays;
 import static java.lang.System.*; 
