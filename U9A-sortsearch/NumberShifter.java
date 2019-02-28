8ef4744188f981b3083fbafc56d9dd9d68427427
Katie Hsieh <katiehsieh25@gmail.com>
2019-02-28 00:36:25 -0800

    NumberShifter

--- U9A-sortsearch/NumberShifter.java
+++ U9A-sortsearch/NumberShifter.java
@@ -11,27 +11,29 @@ import static java.lang.System.*;
 
 public class NumberShifter
 {
-	public static int[] makeLucky7Array(int size)
-	{
-	  int[] nums = new int[size];
-	  
-	  for (int i = 0; i < size; i++) {
-	    nums[i] = (int)(Math.random() * 11);
-	  }
-	  return nums;
-	}
+  public static int[] makeLucky7Array(int size)
+  {
+    int[] nums = new int[size];
+    
+    for (int i = 0; i < size; i++) {
+      nums[i] = (int)(Math.random() * 11);
+    }
 	
-	public static void shiftEm(int[] array)
-	{
-	  int pos = 0;
-	  
-	  for (int i = 0; i < array.length; i++) {
-	    if (array[i] == 7) {
-	      // switch values
-	      array[i] = array[pos];
-	      array[pos] = 7;
-	      pos++;
-	    }
-	  }
-	}
+    return nums;
+  }
+
+  public static void shiftEm(int[] array)
+  {
+    int pos = 0;
+    
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == 7) {
+        // switch values
+        array[i] = array[pos];
+        array[pos] = 7;
+        pos++;
+      }
+    }
+  }
+  
 }
