c7542ab72ff88af682e8d010e3bbabfb01517eab
Katie Hsieh <katiehsieh25@gmail.com>
2019-05-16 17:13:15 -0700

    act7

--- bjacklab/BlackJack.java
+++ bjacklab/BlackJack.java
@@ -4,94 +4,131 @@
 //Lab  - 
 
 import static java.lang.System.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BlackJack
 {
-  private Dealer dealer;
-  private Player player;
+  private ArrayList<Playerable> players;
 
-  public BlackJack()
+  public BlackJack(int numPlayers)
   {
-    dealer = new Dealer();
-    player = new Player();
+    players = new ArrayList<Playerable>();
+    players.add(new Dealer());
+    for (int i = 0; i < numPlayers; i++) {
+      players.add(new Player());
+    }
   }
 
   public void playGame()
   {
+    Dealer dealer = (Dealer)players.get(0);
     Scanner keyboard = new Scanner(System.in);
     char choice = 0;
     
     do {
       dealer.shuffle();
-      dealer.resetHand();
-      player.resetHand();
       
-      for (int i = 0; i < 2; i++) {
-        player.addCardToHand(dealer.deal());
+      for (Playerable player : players) {
+        player.resetHand();
+      }
+      
+      for (int i = 1; i < players.size(); i++) {
+        for (int j = 0; j < 2; j++) {
+          players.get(i).addCardToHand(dealer.deal());
+        }
       }
       
       for (int i = 0; i < 2; i++) {
         dealer.addCardToHand(dealer.deal());
       }
       
-      //PLAYER
-      out.println("\n--- PLAYER ---");
-      out.println("\nCurrent " + player);
-      char hit = 'Y';
-      while (player.hit() && (hit == 'Y' || hit == 'y')) {
+      //PLAYERS
+      for (int i = 1; i < players.size(); i ++) {
+        Player player = (Player)players.get(i);
+        out.println("\n--- PLAYER " + i + " ---");
+        out.println("\nCurrent " + player);
         
-        out.print("Do you want to hit? [Y/N] :: ");
-        hit = keyboard.next().charAt(0);
+        char hit = 'Y';
+        while (player.hit() && (hit == 'Y' || hit == 'y')) {
+          
+          out.print("Do you want to hit? [Y/N] :: ");
+          hit = keyboard.next().charAt(0);
+          
+          if (hit == 'Y' || hit == 'y') {
+            player.addCardToHand(dealer.deal());
+            out.println("\nCurrent " + player);
+          }
+        }
         
-        if (hit == 'Y' || hit == 'y') {
-          player.addCardToHand(dealer.deal());
-          out.println("\nCurrent " + player);
+        if (player.getHandValue() > 21) {
+          out.println("Player " + i + " busted!");
         }
       }
       
-      //DELAER
+      //DEALER
       out.println("\n--- DEALER ---");
       out.println("\nDealer " + dealer);
-      while (dealer.hit() && player.hit()) {
+      
+      while (dealer.hit()) {
          dealer.addCardToHand(dealer.deal());
          out.println("Dealer " + dealer);
       }
       
+      if (dealer.getHandValue() > 21) {
+        out.println("Dealer busted!");
+      }
+      
+      //RESULTS
       out.println("\n--- RESULTS ---");
       
-      out.println("\nPLAYER");
-      out.println("Hand Value :: " + player.getHandValue());
-      out.println("Hand Size :: " + player.getHandSize());
-      out.println("Cards in Hand :: " + player);
+      for (int i = 1; i < players.size(); i++) {
+        Player player = (Player)players.get(i);
+        out.println("\nPLAYER " + i);
+        out.println("Hand Value :: " + player.getHandValue());
+        out.println("Hand Size :: " + player.getHandSize());
+        out.println("Cards in Hand :: " + player);
+      }
       
       out.println("\nDEALER");
       out.println("Hand Value :: " + dealer.getHandValue());
       out.println("Hand Size :: " + dealer.getHandSize());
       out.println("Cards in Hand :: " + dealer);
       
-      if (player.getHandValue() > 21) {
-        out.println("\nDealer wins - Player busted!\n");
-        dealer.addWinCount();
-      }
-      else if (dealer.getHandValue() > 21) {
-        out.println("\nPlayer wins - Dealer busted!\n");
-        player.addWinCount();
-      }
-      else if (player.getHandValue() > dealer.getHandValue()) {
-        out.println("\nPlayer has bigger hand value!\n");
-        player.addWinCount();
-      }
-      else if (dealer.getHandValue() > player.getHandValue()) {
-        out.println("\nDealer has bigger hand value!\n");
-        dealer.addWinCount();
-      }
-      else {
-        out.println("\nTie!\n");
+      //WINNNERS
+      out.println("\nWINNERS");
+      
+      int highest = 0;
+      for (int i = 0; i < players.size(); i++) {
+        int val = players.get(i).getHandValue();
+        if (val <= 21 && val > highest) {
+          highest = val;
+        }
       }
       
-      out.println("Dealer has won " + dealer.getWinCount() + " times.");
-      out.println("Player has won " + player.getWinCount() + " times.");
+      if (highest == 0) {
+        out.println("No winner - all players busted!");
+      }
+      else {
+        for (int i = 1; i < players.size(); i++) {
+          if (players.get(i).getHandValue() == highest) {
+            out.println("Player " + i + " wins!");
+            Player winner = (Player)players.get(i);
+            winner.addWinCount();
+          }
+        }
+        
+        if (dealer.getHandValue() == highest) {
+          out.println("Dealer wins!");
+          dealer.addWinCount();
+        }
+      }
+      
+      out.println("\nDealer has won " + dealer.getWinCount() + " times.");
+      for (int i = 1; i < players.size(); i++) {
+        out.println("Player " + i + " has won " + players.get(i).getWinCount() + " times.");
+      }
       
       out.print("\nDo you want to play again? [Y/N] :: ");
       choice = keyboard.next().charAt(0);
@@ -102,7 +139,14 @@ public class BlackJack
         
   public static void main(String[] args)
   {
-    BlackJack game = new BlackJack();
+    Scanner keyboard = new Scanner(System.in);
+    int numPlayers;
+    do {
+      out.print("How many players? :: ");
+      numPlayers = keyboard.nextInt();
+    } while (numPlayers < 1);
+    
+    BlackJack game = new BlackJack(numPlayers);
     game.playGame();
   }
 }
